d7cfb8a9398c526aa0abc89158b68a7fdc6a1c28
SeonJungKim <ksj083895@gmail.com>
2021-04-22 11:48:04 +0500

    Initial Commit

---
+++ SwiftUI_Path_TUT/SwiftUI_Path_TUTApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Path_TUTApp.swift
+//  SwiftUI_Path_TUT
+//
+//  Created by 김선중 on 2021/04/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Path_TUTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
