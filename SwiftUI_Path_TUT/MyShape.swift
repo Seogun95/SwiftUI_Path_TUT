09ba8d46bc7377201b9b3b85b644a09d419de340
SeonJungKim <ksj083895@gmail.com>
2021-04-23 18:27:37 +0500

    물방울 모양 그리기

--- SwiftUI_Path_TUT/MyShape.swift
+++ SwiftUI_Path_TUT/MyShape.swift
@@ -8,6 +8,19 @@
 import SwiftUI
 
 
+struct Raindrop: Shape {
+    func path(in rect: CGRect) -> Path {
+        Path { path in
+            path.move(to: CGPoint(x: rect.size.width/2, y: 0))
+           
+            path.addQuadCurve(to: CGPoint(x: rect.size.width/2, y: rect.size.height), control: CGPoint(x: rect.size.width, y: rect.size.height))
+            
+            path.addQuadCurve(to: CGPoint(x: rect.size.width/2, y: 0), control: CGPoint(x: 0, y: rect.size.height))
+        }
+    }
+}
+
+
 struct MySquare: Shape {
     func path(in rect: CGRect) -> Path {
         var path = Path()
@@ -24,8 +37,9 @@ struct MySquare: Shape {
 
 struct MyShape: View {
     var body: some View {
-        MySquare()
-            .frame(width: 250, height: 250)
+       Raindrop()
+        .fill(LinearGradient(gradient: Gradient(colors: [Color.white, Color.blue]), startPoint: .topLeading, endPoint: .bottom))
+        .frame(width: 200, height: 200)
     }
 }
 
---
+++ SwiftUI_Path_TUT/Raindrop.swift
@@ -0,0 +1,21 @@
+//
+//  Raindrop.swift
+//  SwiftUI_Path_TUT
+//
+//  Created by 김선중 on 2021/04/23.
+//
+
+import SwiftUI
+
+
+struct Raindrop: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Raindrop_Previews: PreviewProvider {
+    static var previews: some View {
+        Raindrop()
+    }
+}
